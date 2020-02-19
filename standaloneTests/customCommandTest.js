ba11dc7e796fab93a3c9a0272922f8a5aafc0aa0
Harshgiri Gonsai <giri@Harshgiris-MacBook-Pro.local>
2020-02-20 00:44:03 +1300

    Adding 'expect' assertions to 'customCommands'

--- standaloneTests/customCommandTest.js
+++ standaloneTests/customCommandTest.js
@@ -1,6 +1,6 @@
-//const expect = require('expect'); 
-
-beforeEach('Running custom commands', async() => {
+const expect = require('chai').expect;
+ 
+ beforeEach('Running custom commands', async() => {
     await browser.url('/Accordion/index.html');
     browser.setWindowSize(1800, 1200);
 });
@@ -8,13 +8,10 @@ beforeEach('Running custom commands', async() => {
 describe('Custom command test', () => {
 
     it('Test of running custom command', () => {
-        //const accordianLink = $("#///h1[@innertext='ACCORDION & TEXT AFFECTS (APPEAR & DISAPPEAR)']");
-        //accordianLink.click();
-
         //Custom Command Method
         const customCommandPageData = browser.getUrlAndTitle();
 
-        //expect(customCommandPageData.title).to.contains('Accordian');
-        //expect(customCommandPageData.url).to.contains('Accordian');
+        expect(customCommandPageData.title).to.contains('Accordion Items');
+        expect(customCommandPageData.url).to.contains('Accordion');
     });
 });
