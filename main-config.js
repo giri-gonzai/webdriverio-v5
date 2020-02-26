e25eb66cd81b74408709e284c834dad546f235b5
Harshgiri Gonsai <giri@Harshgiris-MacBook-Pro.local>
2020-02-26 17:19:05 +1300

    Adding config file for separating mock users & testing environment

---
+++ main-config.js
@@ -0,0 +1,10 @@
+//Not to be implemented as the POM Framework follows abstraction properties.
+
+module.exports = {
+    browser: 'chrome',
+    baseUrl: 'http://www.webdriveruniversity.com',
+    logLevel: 'info',
+    firstName: 'NA',
+    lastName: 'NA',
+    timeout: 15000
+}
